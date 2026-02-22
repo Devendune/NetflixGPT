e21b06240bec7567623054930f12f1d88dfcb08e
Devendune <devendunegi06@gmail.com>
2026-02-22 10:45:26 +0530

    added Sign up form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 const Header=()=>{
     return(
-        <div className="logo p-6">
-            <img src="https://images.ctfassets.net/y2ske730sjqp/821Wg4N9hJD8vs5FBcCGg/9eaf66123397cc61be14e40174123c40/Vector__3_.svg?w=460" alt="logo"/>
+        <div className="absolute pt-4 pl-4">
+            <img className="w-32 h-16" src="https://images.ctfassets.net/y2ske730sjqp/821Wg4N9hJD8vs5FBcCGg/9eaf66123397cc61be14e40174123c40/Vector__3_.svg?w=460" alt="logo"/>
         </div>
     )
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -3,9 +3,16 @@ import { useState } from "react";
 import Header from "./Header";
 const Login = () => {
     return (
-        <div className="background-img">
+        <div className="header page">
             <Header/>
+            <div className="">
             <img src="https://assets.nflxext.com/ffe/siteui/vlv3/37372b0c-16ef-4614-9c66-f0464ffe4136/web/IN-en-20260216-TRIFECTA-perspective_74aa38a5-f527-417e-a604-a039567a350b_medium.jpg" alt="background" />
+            </div>
+            <form className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center bg-black bg-opacity-80 p-8 rounded-lg w-80">
+                <input type="text" placeholder="username" className="p-3 m-3 w-full rounded"/>
+                <input type="password" placeholder="password" className="p-3 m-3 w-full rounded"/>
+                <button className="p-3 m-3 w-full bg-red-600 text-white rounded hover:bg-red-700">Sign In</button>
+            </form>
         </div>
     )
 };
