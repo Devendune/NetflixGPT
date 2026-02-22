768cc34c0a062c8ec99e24abf39ed88a7cc6f4b5
Devendune <devendunegi06@gmail.com>
2026-02-22 10:10:48 +0530

    added code for Netlfix logo and background img

---
+++ src/components/Browse.js
@@ -0,0 +1,9 @@
+const Browse=()=>{
+    return(
+        <div className="text-3xl font-bold text-green-500">
+            Browse Page
+        </div>
+    )
+}
+
+export default Browse;
---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useState } from "react";
+import Header from "./Header";
+const Login = () => {
+    return (
+        <div className="background-img">
+            <Header/>
+            <img src="https://assets.nflxext.com/ffe/siteui/vlv3/37372b0c-16ef-4614-9c66-f0464ffe4136/web/IN-en-20260216-TRIFECTA-perspective_74aa38a5-f527-417e-a604-a039567a350b_medium.jpg" alt="background" />
+        </div>
+    )
+};
+
+export default Login;
