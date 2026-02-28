25bbed6f8792e969a077ffc8b00f7f72e76d718a
Devendune <devendunegi06@gmail.com>
2026-02-28 14:15:22 +0530

    fetched the trailer for movie

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import { options } from '../utils/options';
-import  useGetMovieVideos  from '../hooks/useGetMovieVideos';
+import useGetMovieVideos from '../hooks/useGetMovieVideos'; // Remove {} for default import
 const VideoBackground = ({movieId}) => 
   {
-    const videoData=useGetMovieVideos(movieId);
-    console.log(videoData);
+    const trailer = useGetMovieVideos(movieId);
+    console.log("Trailer data:", trailer);
+    
   return (
     <div>
         <h1>Video Background</h1>
+        <p>Movie ID: {movieId}</p>
+        <div>
+          <iframe 
+          width="560" 
+          height="315" 
+          src="https://www.youtube.com/embed/auHiLFjaIc0?si=HLHcGci_gr-ae5tq"
+          title="YouTube video player" 
+          frameBorder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" 
+          allowFullScreen>
+          </iframe>
+        </div>
     </div>
   )
 }
--- src/hooks/useGetMovieVideos.js
+++ src/hooks/useGetMovieVideos.js
@@ -1,17 +1,28 @@
 import { options } from "../utils/options";
-import { useEffect } from "react";
-const useGetMovieVideos=(movieId)=>
-{
-    const getMovieVideos=async (movieId)=>{
-        const data=await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`,options
-        );
-        const jsonData=await data.json();
-        console.log("Video Data",jsonData); 
+import { useEffect, useState } from "react";
+
+const useGetMovieVideos = (movieId) => {
+    const [trailerVideo, setTrailerVideo] = useState(null);
+
+    const getMovieVideos = async (movieId) => {
+        const data = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`, options);
+        const jsonData = await data.json();
+        console.log("All videos:", jsonData.results);
+        
+        // Find trailer video
+        const filterData = jsonData.results.filter(video => video.type === "Trailer");
+        const trailer = filterData.length ? filterData[0] : jsonData.results[0];
+        
+        setTrailerVideo(trailer);
     };
 
-    useEffect(()=>{
-        getMovieVideos(movieId);
-    },[]);
+    useEffect(() => {
+        if (movieId) {
+            getMovieVideos(movieId);
+        }
+    }, [movieId]);
+
+    return trailerVideo; // Return the trailer data
 }
 
 export default useGetMovieVideos;
